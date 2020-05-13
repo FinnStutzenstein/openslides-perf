ab33e4f6cf7eade867510bd5a5b6e0478fd87772
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2020-05-13 08:47:09 +0200

    wip

--- locust/login.py
+++ locust/login.py
@@ -1,4 +1,3 @@
-import json
 from locust import HttpLocust, TaskSet, between, task
 from requests.exceptions import ConnectionError
 from locust.contrib.fasthttp import FastHttpLocust
@@ -11,9 +10,10 @@ class UserBehavior(TaskSet):
         self.logged_in = False
         super().__init__(*args, **kwargs)
 
+    @task()
     def login(self):
         if self.logged_in:
-            return True
+            return
         name = f"user{self.id}"
         print(f"login {name}")
         data = {"username": name, "password": name}
@@ -24,50 +24,15 @@ class UserBehavior(TaskSet):
                     if whoami["user"] is None:
                         res.failure("Was not logged in")
                     else:
-                        self.present = whoami["user"]["is_present"]
-                        self.cookies = res.cookies
-                        self.logged_in = True
                         res.success()
+                        self.logged_in = True
                 else:
                     res.failure(f"not ok: {res.status_code}")
             except ConnectionError:
                 res.failure("ConnectionError")
-
-        return self.logged_in
-
-    def on_start(self):
-        pass
 
     def on_stop(self):
         USERS.append(self.id)
-
-    @task()
-    def toggle_present(self):
-        if not self.login():
-            return
-        print(f"User: {self.id}")
-        with self.json_post("/apps/users/setpresence/", not self.present) as res:
-            try:
-                if res.ok:
-                    self.present = not self.present
-                    res.success()
-                else:
-                    print(res.content)
-                    res.failure(f"not ok: {res.status_code}")
-            except ConnectionError:
-                res.failure("ConnectionError")
-
-    def json_post(self, url, data):
-        headers = {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': self.cookies.get('OpenSlidesCsrfToken')
-        }
-        return self.client.post(
-            url,
-            data=json.dumps(data),
-            headers=headers,
-            catch_response=True,
-            cookies=self.cookies)
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
